4a2de79826649fa19387d8f61a07bfcfe2932363
Danni-Du <72844387+Danni-Du@users.noreply.github.com>
2025-03-10 19:43:49 -0400

    Update MOM_oda_ml.F90

--- src/ocean_data_assim/MOM_oda_ml.F90
+++ src/ocean_data_assim/MOM_oda_ml.F90
@@ -61,7 +61,7 @@ real :: reference_depth = 10
 real :: ReLU_zero = 0
 real, dimension(15) :: target_sigmas = (/0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5,1.7,1.9,2.1,2.3,2.5,2.7,2.9/)
 real, dimension(16) :: output_flux_sigmas = (/0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0/)
-character(len=255)  :: danni_ANN_name = '/gpfs/f5/gfdl_sd/world-shared/Feiyu.Lu/ECDA_data/ML/danni_ANN_M4_TzBzdivmldtaufluxUz_2003_2016_20epoch.nc'
+character(len=255)  :: danni_ANN_name = '/gpfs/f5/gfdl_sd/world-shared/Danni.Du/ECDA_data/ML/danni_ANN_M4_TzBzdivmldtaufluxUz_2003_2016_20epoch.nc'
 real :: seconds_in_30_days = 3600*24*30
 
 integer :: id_clock_ml_remapping
